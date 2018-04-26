d803d078cb327490c80ad63dbd2153c35e5e1fa5
S. Seegel <post@seegel-systeme.de>
2018-04-26 22:44:49 +0000

    add api test

---
+++ spitest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2.7
+
+import RPi.GPIO as GPIO
+import spidev
+import random
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
+
+random.seed()
+
+dataout = []
+
+for i in range(32):
+	dataout.append(random.randint(0, 255))
+
+print "Transfering testdata", dataout
+datain = spi.xfer2(dataout)
+
+if dataout == datain:
+	print "Received data OK!"
+
+spi.close()
