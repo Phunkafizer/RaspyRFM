5063f129a9e799f42820a34f163cf964a97049f9
Stefan Seegel <post@seegel-systeme.de>
2020-11-03 10:15:33 +0000

    refactor for rfm pythonmodule

--- apps/emoncms.py
+++ apps/emoncms.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2.7
 
-import rfm69
-from rfm69 import Rfm69
+from raspyrfm import *
 import sensors
 from sensors import rawsensor
 import time
@@ -10,44 +9,37 @@ import json
 
 
 URL = 'https://emoncms.org/input/post.json'
-APIKEY = '12345678901234567890'
+APIKEY = '123456789123456789'
 
 sensors = [
 	{
-		"sensorId": 'b8', #ID of the sensor
+		"sensorId": '68', #ID of the sensor
 		"minInterval": 30, #if new value received before <minInterval> in seconds, value is discarded
 		"node": 1 #node number in emoncms
 	},
 	{
-		"sensorId": 'e4',
+		"sensorId": '8',
 		"minInterval": 30,
 		"node": 2
 	},
 	{
-		"sensorId": 'ec',
+		"sensorId": '98',
 		"minInterval": 30,
 		"node": 3
 	},
   {
-		"sensorId": '68',
+		"sensorId": '18',
 		"minInterval": 30,
 		"node": 4
-	},
-  {
-		"sensorId": '4',
-		"minInterval": 30,
-		"node": 5
-	},
-  {
-		"sensorId": '78',
-		"minInterval": 30,
-		"node": 6
-	}	
+	}
 ]
 
 lasttimes = {}
 
 def LogSensor(data):
+    if not ('ID' in data):
+        return
+
     for s in sensors: #search for received sensor in list
         if s['sensorId'] == data['ID']:
             if data['ID'] in lasttimes:
@@ -68,15 +60,15 @@ def LogSensor(data):
     print "No match for ID"
 
 
-if Rfm69.Test(1):
-    rfm = Rfm69(1, 24) #when using the RaspyRFM twin
-elif Rfm69.Test(0):
-    rfm = Rfm69() #when using a single single 868 MHz RaspyRFM
+if raspyrfm_test(2, RFM69):
+    rfm = RaspyRFM(2, RFM69) #when using the RaspyRFM twin
+elif raspyrfm_test(1, RFM69):
+    rfm = RaspyRFM(1, RFM69) #when using a single single 868 MHz RaspyRFM
 else:
     print "No RFM69 module found!"
     exit()
 
-rfm.SetParams(
+rfm.set_params(
     Freq = 868.300, #MHz center frequency
     Datarate = 9.579, #17.241, #kbit/s baudrate
     Deviation = 30, #30 kHz
@@ -90,7 +82,7 @@ rfm.SetParams(
     )
     
 while 1:
-    data = rfm.ReceivePacket(7)
+    data = rfm.receive(7)
     obj = rawsensor.CreateSensor(data)
     print(str(obj))
     if len(obj._data) > 0:
